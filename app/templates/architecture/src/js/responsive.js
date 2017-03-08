166f966c6e516eb35c2879cb535ad82bc8ee333f
Joe Maag <jmaag@rapiddg.com>
2017-03-08 13:40:12 -0500

    comments

--- app/templates/architecture/src/js/responsive.js
+++ app/templates/architecture/src/js/responsive.js
@@ -1,31 +1,49 @@
 (function ($, Drupal, window, document, undefined) {
 
   /*
-  Execute javascript based on css @media breakpoints, require Match Media polyfill for IE9
-
-  ** Enquire.js utilizes the matchMedia Javascript API:
+  Execute javascript based on css @media breakpoints,
+  require Match Media polyfill for IE9
+  Note: Enquire.js utilizes the matchMedia Javascript API:
   Using enquire JS http://wicky.nillia.ms/enquire.js/
   https://developer.mozilla.org/en-US/docs/Web/API/Window.matchMedia
   https://developer.mozilla.org/en-US/docs/Web/Guide/CSS/Testing_media_queries?redirectlocale=en-US&redirectslug=DOM%2FUsing_media_queries_from_code
-
-  Typical theme breakpoints:
-  Minimum Width: 320px / 20em;
-  Phone: 480px / 30em;
-  Tablet Small: 600px / 37.5em;
-  Tablet: 760px / 47.5em;
-  Desktop: 980px / 61.25em;
-
   */
 
   $(document).ready(function() {
 
-    // Enquire Variables
-    var breakMobile = 'screen and (max-width:47.499em)';
+    var toggleMenu = {
+
+      init : function () {
+        var mainNavToggle = $('.toggle-nav'),
+            $body = $('body');
+
+        $(mainNavToggle).on('click', function(event){
+          event.preventDefault();
+          event.stopPropagation();
+
+          $body.toggleClass('show-nav');
+        });
+
+      },
+      destroy : function () {
+        $('body').removeClass('show-nav');
+      }
+    };
+
 
     // Functions to be executed on all breakpoints
     // ...place code here ...
 
-    enquire.register(breakMobile, {
+
+    // Typical theme breakpoints:
+    // Minimum Width: 320px / 20em;
+    // Phone: 480px / 30em;
+    // Tablet Small: 600px / 37.5em;
+    // Tablet: 760px / 47.5em;
+    // Desktop: 980px / 61.25em;
+    var breakpointMobile = 'screen and (max-width:47.499em)';
+
+    enquire.register(breakpointMobile, {
 
       // OPTIONAL
       // If supplied, triggered when a media query matches.
@@ -61,24 +79,5 @@
     });
 
   });  // End Ready
-
-  var toggleMenu = {
-
-    init : function () {
-      var mainNavToggle = $('.toggle-nav'),
-          $body = $('body');
-
-      $(mainNavToggle).on('click', function(event){
-        event.preventDefault();
-        event.stopPropagation();
-
-        $body.toggleClass('show-nav');
-      });
-
-    },
-    destroy : function () {
-      $('body').removeClass('show-nav');
-    }
-  };
 
 })(jQuery, Drupal, this, this.document);
